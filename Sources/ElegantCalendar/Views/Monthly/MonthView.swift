a62f58d84bdcd126aaddbd0e35e4f5963bde2b1a
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2020-11-17 21:46:09 +0300

    remove accessory view from month view

--- Sources/ElegantCalendar/Views/Monthly/MonthView.swift
+++ Sources/ElegantCalendar/Views/Monthly/MonthView.swift
@@ -29,11 +29,6 @@ struct MonthView: View, MonthlyCalendarManagerDirectAccess {
                 .padding(.leading, CalendarConstants.Monthly.outerHorizontalPadding)
                 .onTapGesture { self.communicator?.showYearlyView() }
             weeksViewWithDaysOfWeekHeader
-            if selectedDate != nil {
-                calenderAccessoryView
-                    .padding(.leading, CalendarConstants.Monthly.outerHorizontalPadding)
-                    .id(selectedDate!)
-            }
             Spacer()
         }
         .padding(.top, CalendarConstants.Monthly.topPadding)
@@ -98,87 +93,6 @@ private extension MonthView {
                 WeekView(calendarManager: self.calendarManager, week: week)
             }
         }
-    }
-
-}
-
-private extension MonthView {
-
-    var calenderAccessoryView: some View {
-        CalendarAccessoryView(calendarManager: calendarManager)
-    }
-
-}
-
-private struct CalendarAccessoryView: View, MonthlyCalendarManagerDirectAccess {
-
-    let calendarManager: MonthlyCalendarManager
-
-    @State private var isVisible = false
-
-    private var numberOfDaysFromTodayToSelectedDate: Int {
-        let startOfToday = calendar.startOfDay(for: Date())
-        let startOfSelectedDate = calendar.startOfDay(for: selectedDate!)
-        return calendar.dateComponents([.day], from: startOfToday, to: startOfSelectedDate).day!
-    }
-
-    private var isNotYesterdayTodayOrTomorrow: Bool {
-        abs(numberOfDaysFromTodayToSelectedDate) > 1
-    }
-
-    var body: some View {
-        VStack {
-            selectedDayInformationView
-            GeometryReader { geometry in
-                self.datasource?.calendar(viewForSelectedDate: self.selectedDate!,
-                                          dimensions: geometry.size)
-            }
-        }
-        .onAppear(perform: makeVisible)
-        .opacity(isVisible ? 1 : 0)
-        .animation(.easeInOut(duration: 0.5))
-    }
-
-    private func makeVisible() {
-        isVisible = true
-    }
-
-    private var selectedDayInformationView: some View {
-        HStack {
-            VStack(alignment: .leading) {
-                dayOfWeekWithMonthAndDayText
-                if isNotYesterdayTodayOrTomorrow {
-                    daysFromTodayText
-                }
-            }
-            Spacer()
-        }
-    }
-
-    private var dayOfWeekWithMonthAndDayText: some View {
-        let monthDayText: String
-        if numberOfDaysFromTodayToSelectedDate == -1 {
-            monthDayText = "Yesterday"
-        } else if numberOfDaysFromTodayToSelectedDate == 0 {
-            monthDayText = "Today"
-        } else if numberOfDaysFromTodayToSelectedDate == 1 {
-            monthDayText = "Tomorrow"
-        } else {
-            monthDayText = selectedDate!.dayOfWeekWithMonthAndDay
-        }
-
-        return Text(monthDayText.uppercased())
-            .font(.subheadline)
-            .bold()
-    }
-
-    private var daysFromTodayText: some View {
-        let isBeforeToday = numberOfDaysFromTodayToSelectedDate < 0
-        let daysDescription = isBeforeToday ? "DAYS AGO" : "DAYS FROM TODAY"
-
-        return Text("\(abs(numberOfDaysFromTodayToSelectedDate)) \(daysDescription)")
-            .font(.system(size: 10))
-            .foregroundColor(.gray)
     }
 
 }
